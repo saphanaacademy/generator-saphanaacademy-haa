9f5530d2bccfd936bc072106f67e391d15080ac6
SAP HANA Academy <hanaacademy@sap.com>
2020-05-20 12:03:54 +0200

    add role collection & SSO sub-generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,94 +1,104 @@
-'use strict';
-const Generator = require("yeoman-generator"),
-  path = require("path"),
-  glob = require("glob");
+"use strict";
+const Generator = require("yeoman-generator");
+const path = require("path");
+const glob = require("glob");
 
 module.exports = class extends Generator {
-
   prompting() {
-    return this.prompt([{
-      type: 'input',
-      name: 'projectName',
-      message: 'What project name would you like?',
-      validate: (s) => {
-        if (/^[a-zA-Z0-9_-]*$/g.test(s)) {
-          return true;
-        }
-        return 'Please only use alphanumeric characters for the project name.';
+    return this.prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: "What project name would you like?",
+        validate: (s) => {
+          if (/^[a-zA-Z0-9_-]*$/g.test(s)) {
+            return true;
+          }
+
+          return "Please only use alphanumeric characters for the project name.";
+        },
+        default: "haa",
       },
-      default: 'haa'
-    },
-    {
-      type: 'confirm',
-      name: 'newDir',
-      message: 'Would you like to create a new directory for this project?',
-      default: true
-    },
-    {
-      type: 'input',
-      name: 'hdiContainerName',
-      message: 'What is the name of your HDI container?',
-      default: 'hdi-container',
-      validate: (s) => {
-        if (/^[a-zA-Z0-9_-]*$/g.test(s)) {
-          return true;
-        }
-        return 'HDI container names contain alphanumeric characters only.';
+      {
+        type: "confirm",
+        name: "newDir",
+        message: "Would you like to create a new directory for this project?",
+        default: true,
       },
-    },
-    {
-      type: 'input',
-      name: 'callingHost',
-      message: 'What is the host of your client application?',
-      default: 'tenant.region.sapanalytics.cloud'
-    },
-    {
-      type: 'confirm',
-      name: 'useNamedUser',
-      message: 'Will you be configuring SSO (implies shadow users in HANA)?',
-      default: true
-    },
-    {
-      type: 'confirm',
-      name: 'multiTenantSupport',
-      message: 'Would you like to add multi-tenant SaaS application support?',
-      default: false
-   }]).then((answers) => {
+      {
+        type: "input",
+        name: "hdiContainerName",
+        message: "What is the name of your HDI container?",
+        default: "hdi-container",
+        validate: (s) => {
+          if (/^[a-zA-Z0-9_-]*$/g.test(s)) {
+            return true;
+          }
+
+          return "HDI container names contain alphanumeric characters only.";
+        },
+      },
+      {
+        type: "input",
+        name: "callingHost",
+        message: "What is the host of your client application?",
+        default: "tenant.region.sapanalytics.cloud",
+      },
+      {
+        type: "confirm",
+        name: "useNamedUser",
+        message: "Will you be configuring SSO (implies shadow users in HANA)?",
+        default: true,
+      },
+      {
+        type: "confirm",
+        name: "multiTenantSupport",
+        message: "Would you like to add multi-tenant SaaS application support?",
+        default: false,
+      },
+    ]).then((answers) => {
       if (answers.newDir) {
         this.destinationRoot(`${answers.projectName}`);
       }
+
       if (answers.multiTenantSupport) {
-        answers.tenantMode = 'shared';
+        answers.tenantMode = "shared";
       } else {
-        answers.tenantMode = 'dedicated';
+        answers.tenantMode = "dedicated";
       }
+
       this.config.set(answers);
     });
   }
 
   writing() {
-    this.sourceRoot(path.join(__dirname, 'templates'));
-    glob.sync('**', {
-      cwd: this.sourceRoot(),
-      nodir: true
-    }).forEach((file) => {
-      const sOrigin = this.templatePath(file);
-      const sTarget = this.destinationPath(file);
-      this.fs.copyTpl(sOrigin, sTarget, this.config.getAll());
-    });
+    this.sourceRoot(path.join(__dirname, "templates"));
+    glob
+      .sync("**", {
+        cwd: this.sourceRoot(),
+        nodir: true,
+      })
+      .forEach((file) => {
+        const sOrigin = this.templatePath(file);
+        const sTarget = this.destinationPath(file);
+        this.fs.copyTpl(sOrigin, sTarget, this.config.getAll());
+      });
   }
 
   install() {
-    this.installDependencies({
+    /*
+    This.installDependencies({
       bower: false,
       npm: true
-    });;
+    });
+    */
   }
 
   end() {
-    this.log('');
-    this.log('You can download the SAP HANA Analytics Adapter WAR file from https://tools.hana.ondemand.com/#hanatools');
-    this.log('');
+    this.log("");
+    this.log(
+      "You can download the SAP HANA Analytics Adapter WAR file from https://tools.hana.ondemand.com/#hanatools"
+    );
+    this.log("");
   }
-
 };
